0d0e85f8b30f8440335b346cecc54bf822c400a3
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-09-12 21:14:17 -0700

    created a pin creation for the player. Next going to create a pin creation for the AI

--- mastermind.rb
+++ mastermind.rb
@@ -1,31 +1,55 @@
 #so Let's set the respective classes
 
-def game_start
-  puts "Welcome to MasterMind. Enter 0 to be the codemaker or 1 to be the codebreaker"
-    if ()
-  puts "Go ahead and select the number of rounds"
+module GameRules
+    def valid_code()
+      until ("1".."6").include?(pin = gets.chomp)
+        puts "Pin is out of range. Enter a number between 1-6"
+      end
+      return pin.to_i  
+  end   
+      #contains the pins that say which numbers you got right
+  end
 
+def game_start()
+  player_role = Player.new(player_choice())
+  rounds = GameChoices.new(round_count())
+  player_role.pin_creation()
+  p player_role 
+  #role = player_role.role == 0 ? 
+  new_game = GameChoices.new("blah")
 end
 
-def player_role()
-  until
+def player_choice()
+    puts "Welcome to MasterMind. Enter 0 to be the codemaker or 1 to be the codebreaker"
+  until (("0".."1").include?(selected_role = gets.chomp))
   end
-  return selected_role
+  selected_role.to_i
 
 end
 
 def round_count()
-  until
+    puts "Go ahead and select the number of rounds in between 8 -12"
+  until ("8".."12").include?(round_amount = gets.chomp)   
   end
-  return selected_rounds
+  p round_amount.to_i
 end
 class Player
-    attr_accessor :name
-    def initialize(name)
-        @name = name
+  include GameRules
+    attr_accessor :role, :code
+    def initialize(role)
+        @role = role
     end
+    
+  def pin_creation
+    code = []
+    @code = code
+    4.times do
+        puts "enter a code with numbers between 1 - 6!"
+        code.push(valid_code())
+  end  
     #has the ability to input a array of numbers and recieves a response
     #from the gamechoices module saying which pins are right
+end
 end
 
 class AI
@@ -38,15 +62,13 @@ class AI
 end
 
 class GameChoices
-    attr_accessor :game
-    def initialize
-        @game = game
-    end
+  attr_accessor :code
+  private
+  def initialize(code)
+      @code = code
+  end
  #This is where the where the player decides to be the codemaker/breaker
     #also this is where the pins "live" and you recieve a response from the included gamechoices module
 end
 
-module GameRules
-    #contains the pins that say which numbers you got right
-end
-
+game_start()
