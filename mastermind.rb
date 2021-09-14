e391f932ecf964e8d6016378fe062cdd0a53d110
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-09-14 00:05:03 -0700

    set up more classes and the game start method which sets up the initial values

--- mastermind.rb
+++ mastermind.rb
@@ -1,23 +1,37 @@
 #so Let's set the respective classes
-
+require 'pry-byebug'
 module GameRules
     def valid_code()
-      until ("1".."6").include?(pin = gets.chomp)
+      until ("1".."6").cover?(pin = gets.chomp) && pin.length == 4
         puts "Pin is out of range. Enter a number between 1-6"
       end
-      return pin.to_i  
-  end   
-      #contains the pins that say which numbers you got right
+      return pin
+    end
+    
+    def round_count()
+      puts "Go ahead and select the number of rounds in between 8 -12"
+    until ("8".."12").include?(round_amount = gets.chomp)   
+    end
+    return round_amount
   end
+  #contains the pins that say which numbers you got right
+end
 
 def game_start()
+  new_game = GameRounds.new(0)
   player_role = Player.new(player_choice())
-  rounds = GameChoices.new(round_count())
-  player_role.pin_creation()
-  p player_role 
-  #role = player_role.role == 0 ? 
-  new_game = GameChoices.new("blah")
+  ai = AI.new("computer")
+  new_game = GameRounds.new(0)
+  new_game.round_count()
+  player_role.role == 0 ? player_role.pin_creation : ai.pin_creation_ai
+  binding.pry
+  round(new_game, player_role, ai)
 end
+
+def round(new_game, player_role, ai)
+
+end
+
 
 def player_choice()
     puts "Welcome to MasterMind. Enter 0 to be the codemaker or 1 to be the codebreaker"
@@ -27,12 +41,6 @@ def player_choice()
 
 end
 
-def round_count()
-    puts "Go ahead and select the number of rounds in between 8 -12"
-  until ("8".."12").include?(round_amount = gets.chomp)   
-  end
-  p round_amount.to_i
-end
 class Player
   include GameRules
     attr_accessor :role, :code
@@ -41,31 +49,59 @@ class Player
     end
     
   def pin_creation
-    code = []
-    @code = code
-    4.times do
-        puts "enter a code with numbers between 1 - 6!"
-        code.push(valid_code())
+        puts "enter a 4 digit code with numbers between 1 - 6!"
+        new_code = valid_code()
+        split = new_code.split('')
+        code = []
+        split.each  { |item| code.push(item.to_i) }
+        @code = code
   end  
     #has the ability to input a array of numbers and recieves a response
     #from the gamechoices module saying which pins are right
-end
+
 end
 
 class AI
-    attr_accessor :ai
+include GameRules
+    attr_accessor :ai, :code
     def initialize(ai)
         @ai = ai
+    end
+    def pin_creation_ai
+    code = []
+    @code = code
+    4.times do
+    code.push(rand(6))
+    end
+    end
+
+    def guess(adjust)
+    code.push(5)
+
     end
         #simple ai that keeps positions it gets right
 
 end
 
-class GameChoices
+class GameRounds
   attr_accessor :code
-  private
+include GameRules
   def initialize(code)
       @code = code
+  end
+
+  def right_spot (guess, pins)
+  feedback = []
+  pins.each_with_index
+  end
+  def wrong_position(guess, pins)
+  feedback = []
+  pins.each_with_index
+  end
+  def correct_pins(guess, pins)
+  if (guess == pins)   
+    return true
+  end
   end
  #This is where the where the player decides to be the codemaker/breaker
     #also this is where the pins "live" and you recieve a response from the included gamechoices module
